5c7abcbcd372ce16efcefac87220304a6b23ab73
Steph Karlsen <becomingsteph@gmail.com>
2022-06-24 16:33:00 -0600

    initial commit

---
+++ src/com/skill/distillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,18 @@
+package com.skill.distillery.foodtruck.app;
+
+import com.skill.distillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+	
+	private FoodTruck fleetOfFoodTrucks[] ;
+	
+
+	public static void main(String[] args) {
+	
+		
+		//user menus with scanners
+		//array up to 5 food trucks
+		//
+	}
+
+}
---
+++ src/com/skill/distillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,7 @@
+package com.skill.distillery.foodtruck.entities;
+
+public class FoodTruck {
+
+}
+
+// from "Structure" on instructions. unique truck id
